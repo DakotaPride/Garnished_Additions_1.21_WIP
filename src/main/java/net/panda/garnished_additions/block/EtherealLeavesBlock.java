020f4c0214ad7ca5749626273a47c6aa38bd0283
Dakota <rjtube13@gmail.com>
2026-01-20 21:06:52 -0600

    fixes for the fixes

--- src/main/java/net/panda/garnished_additions/block/EtherealLeavesBlock.java
+++ src/main/java/net/panda/garnished_additions/block/EtherealLeavesBlock.java
@@ -3,13 +3,17 @@ package net.panda.garnished_additions.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.panda.garnished_additions.init.GarnishedAdditionsItemsInit;
+
+import java.util.Random;
 
 public class EtherealLeavesBlock extends LeavesBlock {
     public EtherealLeavesBlock(Properties properties) {
@@ -18,8 +22,14 @@ public class EtherealLeavesBlock extends LeavesBlock {
 
     @Override
     protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
-        if (stack.canPerformAction(net.neoforged.neoforge.common.ItemAbilities.SHEARS_HARVEST))
-            popResource(level, pos, new ItemStack(GarnishedAdditionsItemsInit.ETHEREAL_LEAF.asItem(), 1));
+        if (stack.canPerformAction(net.neoforged.neoforge.common.ItemAbilities.SHEARS_HARVEST)) {
+            popResource(level, pos, new ItemStack(GarnishedAdditionsItemsInit.ETHEREAL_LEAF.asItem(), 1 + (new Random().nextInt(2))));
+            level.setBlock(pos, Blocks.AIR.defaultBlockState(), 1);
+            spawnDestroyParticles(level, player, pos, state);
+
+            EquipmentSlot equipmentslot = stack.equals(player.getItemBySlot(EquipmentSlot.OFFHAND)) ? EquipmentSlot.OFFHAND : EquipmentSlot.MAINHAND;
+            stack.hurtAndBreak(1, player, equipmentslot);
+        }
 
         return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
     }
--- src/main/java/net/panda/garnished_additions/block/EtherealLogBlock.java
+++ src/main/java/net/panda/garnished_additions/block/EtherealLogBlock.java
@@ -1,7 +1,12 @@
 package net.panda.garnished_additions.block;
 
 import net.dakotapride.garnished.item.hatchet.HatchetUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.common.ItemAbility;
@@ -15,12 +20,18 @@ public class EtherealLogBlock extends RotatedPillarBlock {
 
     @Override
     public @Nullable BlockState getToolModifiedState(BlockState state, UseOnContext context, ItemAbility toolAction, boolean simulate) {
-        if(HatchetUtils.canBeUsedToStripLogs(context.getItemInHand().getItem().getDefaultInstance())) {
+        if (HatchetUtils.canBeUsedToStripLogs(context.getItemInHand().getItem().getDefaultInstance())) {
+            Level level = context.getLevel();
+            Player player = context.getPlayer();
+            BlockPos pos = context.getClickedPos();
+
             if (state.is(GarnishedAdditionsBlocksInit.ETHEREAL_LOG.get())) {
+                level.playSound(player, pos, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1.0F, 1.0F);
                 return GarnishedAdditionsBlocksInit.STRIPPED_ETHEREAL_LOG.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
             }
 
             if (state.is(GarnishedAdditionsBlocksInit.ETHEREAL_WOOD.get())) {
+                level.playSound(player, pos, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1.0F, 1.0F);
                 return GarnishedAdditionsBlocksInit.STRIPPED_ETHEREAL_WOOD.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
             }
         }
